2d51e6cc0aff287279bf1a4b7a20c21582ff94f2
apriego87 <antoniopriego22@gmail.com>
2024-06-06 12:49:45 +0200

    ajustes divs anuncios

--- src/routes/board/+page.svelte
+++ src/routes/board/+page.svelte
@@ -19,71 +19,75 @@
 
 <!-- npm install -g dotenv-cli -->
 
-<div>
-	<Dialog.Root>
-		<Dialog.Trigger>
-			<div class="absolute bottom-5 right-5">
-				<Button variant="default" class="size-16 rounded-full">
-					<Plus class="size-4" />
-				</Button>
-			</div></Dialog.Trigger
+<div class="flex h-[88vh] w-full flex-col items-center justify-start">
+	<div class="h-full w-full p-5">
+		<h1 class="my-5 text-center text-3xl font-bold">Anuncios actuales</h1>
+		<div
+			class="flex h-[90%] flex-col items-center overflow-auto lg:flex-row lg:items-start lg:justify-start"
 		>
-		<Dialog.Content class="w-[80%]">
-			<Dialog.Header>
-				<Dialog.Title>
-					<p>Añade un anuncio</p>
-				</Dialog.Title>
-				<Dialog.Description>
-					<p>Crea un aviso, recordatorio, o simplemente deja un mensaje bonito :)</p>
-				</Dialog.Description>
-			</Dialog.Header>
-			<form method="POST" action="?/create">
-				<div class="grid gap-4 py-4">
-					<div class="grid grid-cols-1 gap-4">
-						<Label for="title">Título</Label>
-						<Input id="title" name="title" />
-					</div>
-					<div class="grid grid-cols-1 gap-4">
-						<Label for="description">Descripción</Label>
-						<Textarea id="description" name="description" />
-					</div>
-					<div class="grid grid-cols-1 gap-4">
-						<Label for="category">Categoría</Label>
-						<Select.Root>
-							<Select.Trigger class="w-full">
-								<Select.Value placeholder="categoría" />
-							</Select.Trigger>
-							<Select.Content>
-								<Select.Item value="informatica">Informática</Select.Item>
-								<Select.Item value="rrhh">RRHH</Select.Item>
-								<Select.Item value="general">General</Select.Item>
-							</Select.Content>
-							<Select.Input name="category" />
-						</Select.Root>
-					</div>
+			{#if data.notes.length === 0}
+				<div class="mt-5 w-full text-center">
+					<p><i>No hay anuncios que mostrar...</i></p>
 				</div>
+			{:else}
+				{#each data.notes as note (note)}
+					<div class="m-5 lg:w-auto">
+						<NoteComponent {note} {data} />
+					</div>
+				{/each}
+			{/if}
+		</div>
+	</div>
 
-				<Dialog.Footer>
-					<Button type="submit" class="w-full">Guardar</Button>
-				</Dialog.Footer>
-			</form>
-		</Dialog.Content>
-	</Dialog.Root>
-</div>
+	<div>
+		<Dialog.Root>
+			<Dialog.Trigger>
+				<div class="absolute bottom-5 right-5">
+					<Button variant="default" class="size-16 rounded-full">
+						<Plus class="size-4" />
+					</Button>
+				</div></Dialog.Trigger
+			>
+			<Dialog.Content class="w-[80%]">
+				<Dialog.Header>
+					<Dialog.Title>
+						<p>Añade un anuncio</p>
+					</Dialog.Title>
+					<Dialog.Description>
+						<p>Crea un aviso, recordatorio, o simplemente deja un mensaje bonito :)</p>
+					</Dialog.Description>
+				</Dialog.Header>
+				<form method="POST" action="?/create">
+					<div class="grid gap-4 py-4">
+						<div class="grid grid-cols-1 gap-4">
+							<Label for="title">Título</Label>
+							<Input id="title" name="title" />
+						</div>
+						<div class="grid grid-cols-1 gap-4">
+							<Label for="description">Descripción</Label>
+							<Textarea id="description" name="description" />
+						</div>
+						<div class="grid grid-cols-1 gap-4">
+							<Label for="category">Categoría</Label>
+							<Select.Root>
+								<Select.Trigger class="w-full">
+									<Select.Value placeholder="categoría" />
+								</Select.Trigger>
+								<Select.Content>
+									<Select.Item value="informatica">Informática</Select.Item>
+									<Select.Item value="rrhh">RRHH</Select.Item>
+									<Select.Item value="general">General</Select.Item>
+								</Select.Content>
+								<Select.Input name="category" />
+							</Select.Root>
+						</div>
+					</div>
 
-<div class="h-full w-full p-5">
-	<h1 class="my-5 text-center text-3xl font-bold">Anuncios actuales</h1>
-	<div class="flex flex-row flex-wrap justify-start">
-		{#if data.notes.length === 0}
-			<div class="mt-5 w-full text-center">
-				<p><i>No hay anuncios que mostrar...</i></p>
-			</div>
-		{:else}
-			{#each data.notes as note (note)}
-				<div class="m-5 w-1/5">
-					<NoteComponent {note} {data} />
-				</div>
-			{/each}
-		{/if}
+					<Dialog.Footer>
+						<Button type="submit" class="w-full">Guardar</Button>
+					</Dialog.Footer>
+				</form>
+			</Dialog.Content>
+		</Dialog.Root>
 	</div>
 </div>
